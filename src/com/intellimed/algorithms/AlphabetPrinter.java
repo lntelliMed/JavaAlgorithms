4a88de62dc483bcf5d0e76e5bd99ba2f3e551dbd
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-08-26 00:21:45 -0400

    Add AlphabetPrinter

---
+++ src/com/intellimed/algorithms/AlphabetPrinter.java
@@ -0,0 +1,16 @@
+package com.intellimed.algorithms;
+
+public class AlphabetPrinter {
+
+	public static void main(String[] args) {
+
+		for (char ch = 'a';  ch <= 'z'; ch++) {
+			System.out.println(ch);
+		}
+		
+//		for (char ch = 'A';  ch <= 'Z'; ch++) {
+//			System.out.println(ch);
+//		}
+	}
+
+}
