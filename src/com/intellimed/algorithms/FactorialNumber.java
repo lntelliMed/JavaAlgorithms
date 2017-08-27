c4c5990a64764019b3055b6f35ea167c4c510c71
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-08-26 22:20:23 -0400

    Include a method for factorial calculation via recursion

--- src/com/intellimed/algorithms/FactorialNumber.java
+++ src/com/intellimed/algorithms/FactorialNumber.java
@@ -5,13 +5,35 @@ public class FactorialNumber {
 
 	public static void main(String[] args) {
 		// ********* See also FactorialBigInteger.java ********* /
-		int number = 4;
+		int number = 3;
+		
+		int fact = findFactorialWithLoop(number);		
+		System.out.println("Factorial of number " + number + " using \"findFactorialWithLoop()\" is " + fact);
+		
+		fact = findFactorialWithRecursion(number);	
+		System.out.println("Factorial of number " + number + " using \"findFactorialWithRecursion()\" is " + fact);
+	}
+
+	private static int findFactorialWithLoop(int number) {
 		int fact = 1;
 		for (int i = 1; i <= number; i++) {
 			fact = fact * i;
 		}
-		
-		System.out.println(fact);
+		return fact;
 	}
+	
+//	private static int findFactorialWithRecursion(int number) {
+//		int fact = 1;
+//		if(number>=1){
+//			fact = number * findFactorialWithRecursion(number-1);
+//		}
+//		return fact;
+//	}
 
+	private static int findFactorialWithRecursion(int number) {
+		if(number == 0){
+			return 1;
+		}
+		return number * findFactorialWithRecursion(number - 1);
+	}
 }
