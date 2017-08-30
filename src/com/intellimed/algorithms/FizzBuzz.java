8f830c6edf8c677cb035e321563e43acefa1a163
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-08-30 13:16:38 -0400

    Add FizzBuzz

---
+++ src/com/intellimed/algorithms/FizzBuzz.java
@@ -0,0 +1,25 @@
+package com.intellimed.algorithms;
+
+public class FizzBuzz {
+	public void fizzBuzz(int x){
+		
+		for (int i = 1; i <= x; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if(i % 3 == 0){
+				System.out.println("Fizz");
+			}else if(i % 5 == 0){
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+				
+		}
+		
+	}
+	public static void main(String[] args) {
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		fizzBuzz.fizzBuzz(15);
+	}
+
+}
